863bece3e15c3bc2974e61dc1151357e93c31e94
ADMIN <ADMIN@DESKTOP-2LPON0P>
2025-08-28 22:53:50 +0530

    TaskScheduler

---
+++ TaskScheduler_Assignment/src/assignment/Task.java
@@ -0,0 +1,7 @@
+
+package assignment;
+
+@FunctionalInterface
+interface Task {
+    void execute(); // represents the work to be done
+}
---
+++ TaskScheduler_Assignment/src/assignment/TaskScheduler.java
@@ -0,0 +1,19 @@
+
+package assignment;
+
+class TaskScheduler {
+    public void scheduleTask(Task task, int intervalInSeconds) {
+        Thread thread = new Thread(() -> {
+            try {
+                while (true) {
+                    task.execute();   // Run the task
+                    Thread.sleep(intervalInSeconds * 1000); // Wait for interval
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Task interrupted: " + e.getMessage());
+            }
+        });
+        thread.start();
+    }
+}
+
