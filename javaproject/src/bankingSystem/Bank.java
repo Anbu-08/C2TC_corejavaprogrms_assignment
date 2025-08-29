275e6f254b4e491662fa614fd65ed5c477534a87
STUDENT <STUDENT@DESKTOP-NEMANVE>
2025-08-29 03:15:09 -0700

    Assignment

---
+++ javaproject/src/bankingSystem/Bank.java
@@ -0,0 +1,17 @@
+package bankingSystem;
+
+
+
+public class Bank {
+	private static int totalAccounts=0;
+	
+	
+	public static void incrementAccount() {
+		totalAccounts++;
+	}
+	
+	public static int getTotalAccounts() {
+		return totalAccounts;
+	}
+
+}
