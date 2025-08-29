8b57db5f9bfdd1755595829b6d07b51ccbaf4920
STUDENT <STUDENT@DESKTOP-NEMANVE>
2025-08-29 02:33:09 -0700

    Thread concept

---
+++ Day14/src/interthreadcommunicaton/Consumer.java
@@ -0,0 +1,25 @@
+
+
+	//Program to demonstrate InterThread Communication - Consumer Thread Class
+	package interthreadcommunicaton;
+
+
+	public class Consumer extends Thread {
+		Q obj; 
+
+		public Consumer(Q obj) {
+			this.obj = obj;
+			start();
+		}
+
+		public void run() {
+				while (true) {
+				try {
+					Thread.sleep(3000);
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				obj.get();
+			}
+		}
+}
